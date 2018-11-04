bcc67cebf81c2044535a43771e99c0339d88076c
Edilson Justiniano <edilsonjustiniano@gmail.com>
2018-11-04 14:25:19 -0200

    Finishing the first version of the model
    - Concluded the initial version of the model
    - Next steps, run it using the biggest dataset

--- code/twitter-analysis.py
+++ code/twitter-analysis.py
@@ -25,6 +25,7 @@ from sklearn.ensemble import VotingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 from sklearn import metrics
 
@@ -69,18 +70,9 @@ def isUnnecessary(text):
     
     return isUnnecessary
 
-
-# Create a function in order to generate the matrix with 0 or 1 for the specific words
-def isWordPresent(text, words):
-    isPresent = False
-    if text in words:
-        isUnnecessary = True
-    
-    return isUnnecessary
-
-
 def isEmojiPresent(text):
     # a_list = ['😤 😢 😭 😦 😧 😨 😩 🤯 😬 😰 😱😍 🤔 🙈 me así, bla es se 😌 ds 💕👭👙 🔪 🗡 ⚔️ ♾ 🏴‍☠️']
+    #😀 😁 😂 🤣 😃 😄 😅 😆 😉 😊 😋 😎 😍 😘 😗 😙 😚 ☺️ 🙂 🤗 🤩 
     if (len(re.findall(r'[^\w\s,]', text)) > 0):
         return True
     return False
@@ -92,11 +84,37 @@ def tokensHasEmoji(tokens):
         
     return False
 
+#happinessEmojis = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘', '😗', '😙', '😚', '☺️', '🙂', '🤗', '🤩', '😛', '😜', '😝', '😏', '🤑', '😲', '😬']
+#sadnessEmojis = ['🤔', '🤨', '😐', '😑', '😶', '🙄', '😣', '😥', '😮', '🤐', '😯', '😪', '😫', '😓', '😔', '😕', '🙃', '☹️', '🙁', '😖', '😞', '😟', '😤', '😢', '😭', '😦', '😧', '😨', '😩', '😰', '😱']
+#results = re.findall(r'[^\w\s,]', 'Ops🤣 😇 🤠 🤡')
+#for iterator in results:
+#    if iterator in happinessEmojis:
+#        print("True")
+#    else:
+#        if iterator in sadnessEmojis:
+#            print("False")
+#print(str(results))
+
+def findEmojisByEmojiList(text, emojis):
+    results = re.findall(r'[^\w\s,]', text)
+    for iterator in results:
+        if iterator in emojis:
+            return True
+    
+    return False
+
+def containsIronicEmoji(tokens, emojis):
+    for token in tokens:
+        if findEmojisByEmojiList(token, emojis):
+            return True
+    
+    return False
+
 ## Read the Dataset into a Panda Dataframe
 # read the data
 
-dataset = pd.read_json('../database/tweetsTest.json', lines=True)
-#dataset = pd.read_json('../database/tweets.json', lines=True)
+#dataset = pd.read_json('../database/tweetsTest.json', lines=True)
+dataset = pd.read_json('../database/tweets.json', lines=True)
 
 # load the data in a dataframe
 data = pd.DataFrame(dataset)
@@ -186,14 +204,15 @@ for k, v in userTweetsDict.items():
 
 badWords = ["suicidal", "suicide", "kill", "myself", "end", "die", "dead", "bullied", "bullyng"]
 positiveWords = ["happy", "happiness", "enjoy", "love", "news", "live"]
+happinessEmojis = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘', '😗', '😙', '😚', '☺️', '🙂', '🤗', '🤩', '😛', '😜', '😝', '😏', '🤑', '😲', '😬', '💝', '❤️', '💓', '✨']
+sadnessEmojis = ['🤔', '🤨', '😐', '😑', '😶', '🙄', '😣', '😥', '😮', '🤐', '😯', '😪', '😫', '😓', '😔', '😕', '🙃', '☹️', '🙁', '😖', '😞', '😟', '😤', '😢', '😭', '😦', '😧', '😨', '😩', '😰', '😱']
 
 matrix = []
 for k, v in userTweetsDict.items():
     for i in range(0, len(v)):
         tweet = v[i].get("text")
         isPositiveTweet = v[i].get("isPositiveTweet")
-        print(tweet)
-        
+
         for tokens in tweet:
             vector = []
             vector.append(k)
@@ -219,20 +238,39 @@ for k, v in userTweetsDict.items():
             else:
                 vector.append(0)
             
+            # Is it ironic
             if isPositiveTweet == True:
                 #vector.append(1)
                 #if hasEmoji == True:
-                vector.append(0)
+                if containsIronicEmoji(tokens, sadnessEmojis):
+                    vector.append(1)
+                    #target (isSuicidal)
+                    vector.append(1)
+                else:
+                    vector.append(0)
+                    #target (isSuicidal)
+                    vector.append(0)
+
+                #vector.append(0)
                 #else:
                 #    vector.append(1)
             else:
-                #vector.append(0)
-                if hasEmoji == True:
+                if containsIronicEmoji(tokens, happinessEmojis):
+                    vector.append(1)
+                    #target (isSuicidal)
                     vector.append(0)
                 else:
+                    vector.append(0)
+                    #target (isSuicidal)
                     vector.append(1)
+                #vector.append(0)
+                #if hasEmoji == True:
+                #    vector.append(0)
+                #else:
+                #    vector.append(1)
                 
             matrix.append(vector)
+            print("Vector: " + str(vector))
             #for token in tokens:
             #    result = lemmer.lemmatize(token)
             #    token = result
@@ -251,8 +289,7 @@ for element in matrix:
     targetValues.append(element[-1])
 
 columns = ["suicidal", "suicide", "kill", "myself", "end", "die", "dead", "bullied", "bullyng", "happy", "happiness", "enjoy", "love", "news", "live"] 
-columns.extend(["hasEmoji", "isSuicidal"])
-print(len(columns))
+columns.extend(["hasEmoji", "isIronic", "isSuicidal"])
 
 dataFrame = pd.DataFrame(dataValues)
 dataFrame.columns = columns
@@ -265,7 +302,7 @@ corr_matrix = dataFrame.corr().abs()
 corr_matrix
 
 # Execute the PCA in order to reduce the problem's dimension
-pca = PCA(n_components=17)
+pca = PCA(n_components=len(columns))
 pca.fit(dataFrame)
 dataPCA = pca.transform(dataFrame)
 print(pca.explained_variance_ratio_)
@@ -282,9 +319,12 @@ to_drop = [column for column in upper.columns if any(upper[column] > 0.75)]
 dataFiltered = dataFrame.drop(dataFrame.columns[to_drop], axis=1)
 dataFiltered['target'] = targetValues
 
+print("Data frame re-mounted after PCA...\n")
+
 ##########################
 ## Let's train our model #
 ##########################
+print("Let`s train the data")
 dataTrain, dataTest = train_test_split(dataFiltered, test_size=0.2)
 # create an cross validation model
 seed = 7
@@ -335,10 +375,10 @@ recall = recall_score(testTargets, predictions)
 precision = precision_score(testTargets, predictions) 
 f1Score = f1_score(testTargets, predictions)
 
-print('Essemble Prediction Accuracy: {0:0.2f}'.format(accuracy))
+print('\nEssemble Prediction Accuracy: {0:0.2f}'.format(accuracy))
 print('Essemble Prediction Recall: {0:0.2f}'.format(recall))
 print('Essemble Prediction Precision: {0:0.2f}'.format(precision))
-print('Essembke Prediction F1 score: {0:0.2f}'.format(f1Score))
+print('Essemble Prediction F1 score: {0:0.2f}'.format(f1Score))
 
 # Confusion matrix
 print(confusion_matrix(testTargets, predictions))
@@ -349,7 +389,7 @@ print(classification_report(testTargets, predictions))
 fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
 roc_auc = metrics.auc(fpr, tpr)
 
-print('Essemble AUC FPR:')
+print('\nEssemble AUC FPR:')
 print(fpr)
 print('Essemble AUC TPR:')
 print(tpr)
@@ -361,29 +401,44 @@ print(metrics.auc(fpr, tpr))
 ######################
 # SVM Classification #
 ######################
-#clf = svm.SVC(kernel='linear', C=1)
-#clf = clf.fit(dataFiltered, targetValues)
-#scores = cross_val_score(clf, dataFiltered, targetValues, cv=5)
-
-#print("SVM Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+clf = svm.SVC(kernel='linear', C=1)
+clf = clf.fit(trainFeatures, trainTargets)
 
 ##################
 # SVM Prediction #
 ##################
 # perform the prediction under the test database
-#predictions = clf.predict(testFeatures)
+predictions = clf.predict(testFeatures)
 
 # Evaluate the results
-#accuracy = accuracy_score(testTargets, predictions)
-#recall = recall_score(testTargets, predictions) 
-#precision = precision_score(testTargets, predictions) 
-#f1Score = f1_score(testTargets, predictions)
+accuracy = accuracy_score(testTargets, predictions)
+recall = recall_score(testTargets, predictions) 
+precision = precision_score(testTargets, predictions) 
+f1Score = f1_score(testTargets, predictions)
+results = cross_val_score(clf, trainFeatures, trainTargets, cv=kfold)
+print('\nSVM Accuracy cross validation: {0:0.2f}'.format(results.mean()))
+print('SVM Accuracy: {0:0.2f}'.format(accuracy))
+print('SVM Recall: {0:0.2f}'.format(recall))
+print('SVM Precision: {0:0.2f}'.format(precision))
+print('SVM F1 score: {0:0.2f}'.format(f1Score))
 
-#print('Essemble Prediction Accuracy: {0:0.2f}'.format(accuracy))
-#print('Essemble Prediction Recall: {0:0.2f}'.format(recall))
-#print('Essemble Prediction Precision: {0:0.2f}'.format(precision))
-#print('Essembke Prediction F1 score: {0:0.2f}'.format(f1Score))
+# Confusion matrix
+print(confusion_matrix(testTargets, predictions))
+print(classification_report(testTargets, predictions))
 
+
+# AUC
+fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
+roc_auc = metrics.auc(fpr, tpr)
+
+print('\nSVM AUC FPR:')
+print(fpr)
+print('SVM AUC TPR:')
+print(tpr)
+print('SVM AUC Thresholds:')
+print(thresholds)
+print('SVM AUC ROC:')
+print(metrics.auc(fpr, tpr))
 
 ############################
 # Decision Tree Prediction #
@@ -398,7 +453,7 @@ recall = recall_score(testTargets, predictions)
 precision = precision_score(testTargets, predictions) 
 f1Score = f1_score(testTargets, predictions)
 
-print('Tree Prediction Accuracy: {0:0.2f}'.format(accuracy))
+print('\nTree Prediction Accuracy: {0:0.2f}'.format(accuracy))
 print('Tree Prediction Recall: {0:0.2f}'.format(recall))
 print('Tree Prediction Precision: {0:0.2f}'.format(precision))
 print('Tree Prediction F1 score: {0:0.2f}'.format(f1Score))
@@ -412,7 +467,7 @@ print(classification_report(testTargets, predictions))
 fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
 roc_auc = metrics.auc(fpr, tpr)
 
-print('Tree AUC FPR:')
+print('\nTree AUC FPR:')
 print(fpr)
 print('Tree AUC TPR:')
 print(tpr)
@@ -422,11 +477,48 @@ print('Tree AUC ROC:')
 print(metrics.auc(fpr, tpr))
 
 
+############################
+# Random Forest Prediction #
+############################
+clf = RandomForestClassifier(n_estimators=30, max_depth=None,
+                             random_state=None)
+clf = clf.fit(trainFeatures, trainTargets)
+predictions = clf.predict(testFeatures)
+
+# Evaluate the results
+accuracy = accuracy_score(testTargets, predictions)
+recall = recall_score(testTargets, predictions) 
+precision = precision_score(testTargets, predictions) 
+f1Score = f1_score(testTargets, predictions)
+
+print('\nRandom Forest Prediction Accuracy: {0:0.2f}'.format(accuracy))
+print('Random Forest Prediction Recall: {0:0.2f}'.format(recall))
+print('Random Forest Prediction Precision: {0:0.2f}'.format(precision))
+print('Random Forest Prediction F1 score: {0:0.2f}'.format(f1Score))
+
+# Confusion matrix
+print(confusion_matrix(testTargets, predictions))
+print(classification_report(testTargets, predictions))
+
+
+# AUC
+fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
+roc_auc = metrics.auc(fpr, tpr)
+
+print('\nRandom Forest AUC FPR:')
+print(fpr)
+print('Random Forest AUC TPR:')
+print(tpr)
+print('Random Forest AUC Thresholds:')
+print(thresholds)
+print('Random Forest AUC ROC:')
+print(metrics.auc(fpr, tpr))
+
 #############################
 # Neural Network Prediction #
 #############################
 neuralNetwork = MLPClassifier(solver='adam', alpha=1e-5,
-                    hidden_layer_sizes=(5, 2), random_state=1)
+                    hidden_layer_sizes=(9, 4), random_state=1)
 neuralNetwork = neuralNetwork.fit(trainFeatures, trainTargets)
 predictions = neuralNetwork.predict(testFeatures)
 
@@ -436,7 +528,7 @@ recall = recall_score(testTargets, predictions)
 precision = precision_score(testTargets, predictions) 
 f1Score = f1_score(testTargets, predictions)
 
-print('Neural Network Prediction Accuracy: {0:0.2f}'.format(accuracy))
+print('\nNeural Network Prediction Accuracy: {0:0.2f}'.format(accuracy))
 print('Neural Network Prediction Recall: {0:0.2f}'.format(recall))
 print('Neural Network Prediction Precision: {0:0.2f}'.format(precision))
 print('Neural Network Prediction F1 score: {0:0.2f}'.format(f1Score))
@@ -450,7 +542,7 @@ print(classification_report(testTargets, predictions))
 fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
 roc_auc = metrics.auc(fpr, tpr)
 
-print('Neural Network AUC FPR:')
+print('\nNeural Network AUC FPR:')
 print(fpr)
 print('Neural Network AUC TPR:')
 print(tpr)
@@ -472,7 +564,7 @@ recall = recall_score(testTargets, predictions)
 precision = precision_score(testTargets, predictions) 
 f1Score = f1_score(testTargets, predictions)
 
-print('GaussianNB Accuracy: {0:0.2f}'.format(accuracy))
+print('\nGaussianNB Accuracy: {0:0.2f}'.format(accuracy))
 print('GaussianNB Recall: {0:0.2f}'.format(recall))
 print('GaussianNB Precision: {0:0.2f}'.format(precision))
 print('GaussianNB F1 score: {0:0.2f}'.format(f1Score))
@@ -486,7 +578,7 @@ print(classification_report(testTargets, predictions))
 fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
 roc_auc = metrics.auc(fpr, tpr)
 
-print('GaussianNB AUC FPR:')
+print('\nGaussianNB AUC FPR:')
 print(fpr)
 print('GaussianNB AUC TPR:')
 print(tpr)
@@ -505,8 +597,8 @@ dtf_hps= GridSearchCV(clf,param_dist,cv=5)
 dtf_hps.fit(trainFeatures, trainTargets)
 predictions = dtf_hps.predict(testFeatures)
 
-print("Best value for max_depth parameter: {0}".format(dtf_hps.best_params_['max_depth']))
-print("Best value for criterion parameter: {0}".format(dtf_hps.best_params_['criterion']))
+#print("Best value for max_depth parameter: {0}".format(dtf_hps.best_params_['max_depth']))
+#print("Best value for criterion parameter: {0}".format(dtf_hps.best_params_['criterion']))
 
 # Evaluate the results
 accuracy = accuracy_score(testTargets, predictions)
@@ -514,7 +606,7 @@ recall = recall_score(testTargets, predictions)
 precision = precision_score(testTargets, predictions) 
 f1Score = f1_score(testTargets, predictions)
 
-print('GridSearch CV Accuracy: {0:0.2f}'.format(accuracy))
+print('\nGridSearch CV Accuracy: {0:0.2f}'.format(accuracy))
 print('GridSearch CV Recall: {0:0.2f}'.format(recall))
 print('GridSearch CV Precision: {0:0.2f}'.format(precision))
 print('GridSearch CV F1 score: {0:0.2f}'.format(f1Score))
@@ -527,7 +619,7 @@ print(classification_report(testTargets, predictions))
 fpr, tpr, thresholds = metrics.roc_curve(testTargets, predictions)
 roc_auc = metrics.auc(fpr, tpr)
 
-print('GridSearch CV AUC FPR:')
+print('\nGridSearch CV AUC FPR:')
 print(fpr)
 print('GridSearch CV AUC TPR:')
 print(tpr)
